344e027436c973da74afe9acc249132bdda19e62
Venugopal <venugopal_chidirala@intuit.com>
2025-11-17 11:25:24 -0500

    Add brand and whatsapp API

---
+++ src/main/java/com/infobip/model/Paging.java
@@ -0,0 +1,41 @@
+package com.infobip.model;
+
+public class Paging {
+    private int page;
+    private int size;
+    private int totalPages;
+    private int totalResults;
+
+    // Getters and setters
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+}
