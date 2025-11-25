851e4dcbded32ae630d5ca197fa457f6f0f0e9f1
Venugopal <venugopal_chidirala@intuit.com>
2025-11-25 08:59:27 -0500

    spotless

--- src/main/java/com/infobip/model/GetNumberPurchaseOrderStatusResponse.java
+++ src/main/java/com/infobip/model/GetNumberPurchaseOrderStatusResponse.java
@@ -7,98 +7,97 @@ public class GetNumberPurchaseOrderStatusResponse {
     private BasicDetails basicDetails;
     private BillingDetails billingDetails;
     private AdditionalData additionalData;
-    
-        /**
-         * Sets basicDetails.
-         *
-         * @param basicDetails the basic details
-         * @return This {@link GetNumberPurchaseOrderStatusResponse} instance.
-         */
-        public GetNumberPurchaseOrderStatusResponse basicDetails(BasicDetails basicDetails) {
-            this.basicDetails = basicDetails;
-            return this;
-        }
 
-        /**
-         * Returns basicDetails.
-         *
-         * @return basicDetails
-         */
-        @JsonProperty("basicDetails")
-        public BasicDetails getBasicDetails() {
-            return basicDetails;
-        }
+    /**
+     * Sets basicDetails.
+     *
+     * @param basicDetails the basic details
+     * @return This {@link GetNumberPurchaseOrderStatusResponse} instance.
+     */
+    public GetNumberPurchaseOrderStatusResponse basicDetails(BasicDetails basicDetails) {
+        this.basicDetails = basicDetails;
+        return this;
+    }
 
-        /**
-         * Sets basicDetails.
-         *
-         * @param basicDetails the basic details
-         */
-        @JsonProperty("basicDetails")
-        public void setBasicDetails(BasicDetails basicDetails) {
-            this.basicDetails = basicDetails;
-        }
+    /**
+     * Returns basicDetails.
+     *
+     * @return basicDetails
+     */
+    @JsonProperty("basicDetails")
+    public BasicDetails getBasicDetails() {
+        return basicDetails;
+    }
 
-        /**
-         * Sets billingDetails.
-         *
-         * @param billingDetails the billing details
-         * @return This {@link GetNumberPurchaseOrderStatusResponse} instance.
-         */
-        public GetNumberPurchaseOrderStatusResponse billingDetails(BillingDetails billingDetails) {
-            this.billingDetails = billingDetails;
-            return this;
-        }
+    /**
+     * Sets basicDetails.
+     *
+     * @param basicDetails the basic details
+     */
+    @JsonProperty("basicDetails")
+    public void setBasicDetails(BasicDetails basicDetails) {
+        this.basicDetails = basicDetails;
+    }
 
-        /**
-         * Returns billingDetails.
-         *
-         * @return billingDetails
-         */
-        @JsonProperty("billingDetails")
-        public BillingDetails getBillingDetails() {
-            return billingDetails;
-        }
+    /**
+     * Sets billingDetails.
+     *
+     * @param billingDetails the billing details
+     * @return This {@link GetNumberPurchaseOrderStatusResponse} instance.
+     */
+    public GetNumberPurchaseOrderStatusResponse billingDetails(BillingDetails billingDetails) {
+        this.billingDetails = billingDetails;
+        return this;
+    }
 
-        /**
-         * Sets billingDetails.
-         *
-         * @param billingDetails the billing details
-         */
-        @JsonProperty("billingDetails")
-        public void setBillingDetails(BillingDetails billingDetails) {
-            this.billingDetails = billingDetails;
-        }
+    /**
+     * Returns billingDetails.
+     *
+     * @return billingDetails
+     */
+    @JsonProperty("billingDetails")
+    public BillingDetails getBillingDetails() {
+        return billingDetails;
+    }
 
-        /**
-         * Sets additionalData.
-         *
-         * @param additionalData the additional data
-         * @return This {@link GetNumberPurchaseOrderStatusResponse} instance.
-         */
-        public GetNumberPurchaseOrderStatusResponse additionalData(AdditionalData additionalData) {
-            this.additionalData = additionalData;
-            return this;
-        }
+    /**
+     * Sets billingDetails.
+     *
+     * @param billingDetails the billing details
+     */
+    @JsonProperty("billingDetails")
+    public void setBillingDetails(BillingDetails billingDetails) {
+        this.billingDetails = billingDetails;
+    }
 
-        /**
-         * Returns additionalData.
-         *
-         * @return additionalData
-         */
-        @JsonProperty("additionalData")
-        public AdditionalData getAdditionalData() {
-            return additionalData;
-        }
+    /**
+     * Sets additionalData.
+     *
+     * @param additionalData the additional data
+     * @return This {@link GetNumberPurchaseOrderStatusResponse} instance.
+     */
+    public GetNumberPurchaseOrderStatusResponse additionalData(AdditionalData additionalData) {
+        this.additionalData = additionalData;
+        return this;
+    }
 
-        /**
-         * Sets additionalData.
-         *
-         * @param additionalData the additional data
-         */
-        @JsonProperty("additionalData")
-        public void setAdditionalData(AdditionalData additionalData) {
-            this.additionalData = additionalData;
-        }
-        
+    /**
+     * Returns additionalData.
+     *
+     * @return additionalData
+     */
+    @JsonProperty("additionalData")
+    public AdditionalData getAdditionalData() {
+        return additionalData;
+    }
+
+    /**
+     * Sets additionalData.
+     *
+     * @param additionalData the additional data
+     */
+    @JsonProperty("additionalData")
+    public void setAdditionalData(AdditionalData additionalData) {
+        this.additionalData = additionalData;
+    }
 }
--- src/test/java/com/infobip/api/InventoryApiTest.java
+++ src/test/java/com/infobip/api/InventoryApiTest.java
@@ -1,26 +1,24 @@
 package com.infobip.api;
 
+import static org.assertj.core.api.BDDAssertions.then;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.infobip.ApiException;
 import com.infobip.model.*;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
+import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.BDDAssertions.then;
-
-public class InventoryApiTest extends ApiTest{
+public class InventoryApiTest extends ApiTest {
 
     @Test
     void shouldOrderNumber() throws ApiException, JsonProcessingException {
-        String givenRequest = "{\"state\":\"CA\",\"npa\":\"123\",\"nxx\":\"456\",\"type\":\"local\",\"applicationId\":\"app-1\",\"entityId\":\"entity-1\",\"quantity\":10}";
+        String givenRequest =
+                "{\"state\":\"CA\",\"npa\":\"123\",\"nxx\":\"456\",\"type\":\"local\",\"applicationId\":\"app-1\",\"entityId\":\"entity-1\",\"quantity\":10}";
         String givenResponse = "{\"orderId\":\"12345\"}";
 
-        setUpSuccessPostRequest(
-                "/numbers/2/purchase-order", givenRequest, givenResponse);
+        setUpSuccessPostRequest("/numbers/2/purchase-order", givenRequest, givenResponse);
 
         Consumer<NumberPurchaseOrderResponse> assertions = (response) -> {
             then(response.getOrderId()).isEqualTo("12345");
@@ -36,9 +34,9 @@ public class InventoryApiTest extends ApiTest{
     @Test
     void shouldGetNumberOrderStatus() throws ApiException, JsonProcessingException {
         String orderId = "12345";
-        String givenResponse = "[{\"basicDetails\":{\"orderId\":\"123\",\"type\":\"234\"},\"billingDetails\":{\"setupFee\":\100.0,\"monthlyFee\":\"200.0\"},\"additionalData\":{\"state\":\"test\"}}]";
-        setUpSuccessGetRequest(
-                "/numbers/2/purchase-order/[" + orderId + "]/status",Map.of(),  givenResponse);
+        String givenResponse =
+                "[{\"basicDetails\":{\"orderId\":\"123\",\"type\":\"234\"},\"billingDetails\":{\"setupFee\":\100.0,\"monthlyFee\":\"200.0\"},\"additionalData\":{\"state\":\"test\"}}]";
+        setUpSuccessGetRequest("/numbers/2/purchase-order/[" + orderId + "]/status", Map.of(), givenResponse);
 
         Consumer<GetNumberPurchaseOrderStatusResponse> assertions = (response) -> {
             then(response.getBasicDetails().getOrderId()).isEqualTo("123");
