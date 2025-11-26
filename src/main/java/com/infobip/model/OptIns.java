9a0d7f50899356ef46e9d5a16f61affbf1546d55
Venugopal <venugopal_chidirala@intuit.com>
2025-11-26 08:58:41 -0500

    Add campaign API

---
+++ src/main/java/com/infobip/model/OptIns.java
@@ -0,0 +1,21 @@
+package com.infobip.model;
+
+public class OptIns {
+    private Keyword keyword;
+
+    /**
+     * Gets the keyword.
+     * @return the keyword
+     */
+    public Keyword getKeyword() {
+        return keyword;
+    }
+
+    /**
+     * Sets the keyword.
+     * @param keyword the keyword to set
+     */
+    public void setKeyword(Keyword keyword) {
+        this.keyword = keyword;
+    }
+}
